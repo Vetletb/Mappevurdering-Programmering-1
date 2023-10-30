d3dad50308f1ce43b3e2df40d1c3cf5a004cc66f
Vetle <vetletb@stud.ntnu.no>
2023-10-30 11:46:53 +0100

    Added constructor and get/set methods

--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -9,4 +9,52 @@ public class TrainDeparture {
   private final LocalTime departureTime;
   private LocalTime delay;
   private String track;
+
+  public TrainDeparture(final String trainNumber, final String line, final String destination,
+                        final LocalTime departureTime, final LocalTime delay, final String track) {
+    this.trainNumber = trainNumber;
+    this.line = line;
+    this.destination = destination;
+    this.departureTime = departureTime;
+    this.delay = delay;
+    this.track = track;
+  }
+
+  @Override
+  public String toString() {
+    return departureTime + " " + line + " " + trainNumber + " " + destination + " " + delay + " " + track;
+  }
+
+  public void addDelay(final int hours, final int minutes) {
+      this.delay = this.delay.plusHours(hours);
+      this.delay = this.delay.plusMinutes(minutes);
+  }
+
+  public void setTrack(String track) {
+      this.track = track;
+  }
+
+  public String getTrainNumber() {
+    return trainNumber;
+  }
+
+  public String getLine() {
+    return line;
+  }
+
+  public String getDestination() {
+    return destination;
+  }
+
+  public LocalTime getDepartureTime() {
+    return departureTime;
+  }
+
+  public LocalTime getDelay() {
+    return delay;
+  }
+
+  public String getTrack() {
+    return track;
+  }
 }
