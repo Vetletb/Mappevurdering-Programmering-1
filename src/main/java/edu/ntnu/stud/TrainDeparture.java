174843f05b11faa35cb379d39615e42e7eae799c
Vetle <vetletb@stud.ntnu.no>
2023-10-30 11:41:49 +0100

    created train class and added variables

---
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -0,0 +1,12 @@
+package edu.ntnu.stud;
+
+import java.time.LocalTime;
+
+public class TrainDeparture {
+  private final String trainNumber;
+  private final String line;
+  private final String destination;
+  private final LocalTime departureTime;
+  private LocalTime delay;
+  private String track;
+}
