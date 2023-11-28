81a88a7d0c43b9839b672914da3f7bdfd716f310
Vetle <vetletb@stud.ntnu.no>
2023-11-28 14:02:19 +0100

    Added validation of constructor parameters

--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -12,47 +12,23 @@ public class TrainDeparture {
 
   public TrainDeparture(int trainNumber, String line, String destination,
                         LocalTime departureTime, int track) {
+    validateTrainNumber(trainNumber);
+    validateLine(line);
+    validateDestination(destination);
+    validateDepartureTime(departureTime);
+
     this.trainNumber = trainNumber;
     this.line = line;
     this.destination = destination;
     this.departureTime = departureTime;
-    this.track = track;
+
+    setTrack(track);
+    setDelay(LocalTime.of(0, 0));
   }
 
   public TrainDeparture(int trainNumber, String line, String destination,
                         LocalTime departureTime) {
     this(trainNumber, line, destination, departureTime, -1);
-  }
-
-  @Override
-  public String toString() {
-    var sb = new StringBuilder();
-    sb.append("Departure time: ").append(departureTime.toString()).append("\n");
-    sb.append("Line: ").append(line).append("\n");
-    sb.append("Train number: ").append(trainNumber).append("\n");
-    sb.append("Destination: ").append(destination).append("\n");
-    if (delay != null) {
-      sb.append("Delay: ").append(delay.toString()).append("\n");
-    }
-    if (track != -1) {
-      sb.append("Track: ").append(track).append("\n");
-    }
-    return sb.toString();
-  }
-
-  public void addDelay(int hours, int minutes) {
-    if (hours <= 0 || minutes <= 0) {
-      throw new IllegalArgumentException("Delay cannot be 0 or negative");
-    }
-    this.delay = this.delay.plusHours(hours);
-    this.delay = this.delay.plusMinutes(minutes);
-  }
-
-  public void setTrack(int track) {
-    if (track <= 0) {
-      throw new IllegalArgumentException("Track cannot be 0 or negative");
-    }
-    this.track = track;
   }
 
   public int getTrainNumber() {
@@ -77,5 +53,69 @@ public class TrainDeparture {
 
   public int getTrack() {
     return track;
+  }
+
+  private void setDelay(LocalTime delay) {
+    if (delay == null) {
+      throw new IllegalArgumentException("Delay cannot be null");
+    }
+    this.delay = delay;
+  }
+
+  public void setTrack(int track) {
+    if (track < 0) {
+      throw new IllegalArgumentException("Track cannot be negative");
+    }
+    this.track = track;
+  }
+
+  @Override
+  public String toString() {
+    var sb = new StringBuilder();
+    sb.append("Departure time: ").append(departureTime).append("\n");
+    sb.append("Line: ").append(line).append("\n");
+    sb.append("Train number: ").append(trainNumber).append("\n");
+    sb.append("Destination: ").append(destination).append("\n");
+    if (delay != null) {
+      sb.append("Delay: ").append(delay).append("\n");
+    }
+    if (track != -1) {
+      sb.append("Track: ").append(track).append("\n");
+    }
+    return sb.toString();
+  }
+
+  public void addDelay(LocalTime delay) {
+    setDelay(this.delay.plusHours(delay.getHour()).plusMinutes(delay.getMinute()));
+  }
+
+  private void validateTrainNumber(int trainNumber) {
+    if (trainNumber < 0) {
+      throw new IllegalArgumentException("Train number cannot be negative");
+    }
+  }
+
+  private void validateLine(String line) {
+    if (line == null) {
+      throw new IllegalArgumentException("Line cannot be null");
+    }
+    if (line.isBlank()) {
+      throw new IllegalArgumentException("Line cannot be blank");
+    }
+  }
+
+  private void validateDestination(String destination) {
+    if (destination == null) {
+      throw new IllegalArgumentException("Destination cannot be null");
+    }
+    if (destination.isBlank()) {
+      throw new IllegalArgumentException("Destination cannot be blank");
+    }
+  }
+
+  private void validateDepartureTime(LocalTime departureTime) {
+    if (departureTime == null) {
+      throw new IllegalArgumentException("Departure time cannot be null");
+    }
   }
 }
