d468856b7006c115cd201708cbc25c36dd5098cd
Vetle <vetletb@stud.ntnu.no>
2023-11-29 17:19:21 +0100

    Added JavaDoc to class and methods

--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -2,6 +2,9 @@ package edu.ntnu.stud;
 
 import java.time.LocalTime;
 
+/**
+ * This class represents a train departure.
+ */
 public class TrainDeparture {
   private final int trainNumber;
   private final String line;
@@ -10,6 +13,15 @@ public class TrainDeparture {
   private LocalTime delay;
   private int track;
 
+  /**
+   * Creates a new train departure.
+   *
+   * @param trainNumber   the train number
+   * @param line          the line
+   * @param destination   the destination
+   * @param departureTime the departure time
+   * @param track         the track
+   */
   public TrainDeparture(int trainNumber, String line, String destination,
                         LocalTime departureTime, int track) {
     validateTrainNumber(trainNumber);
@@ -26,49 +38,98 @@ public class TrainDeparture {
     setDelay(LocalTime.of(0, 0));
   }
 
+  /**
+   * Creates a new train departure.
+   *
+   * @param trainNumber   the train number
+   * @param line          the line
+   * @param destination   the destination
+   * @param departureTime the departure time
+   */
   public TrainDeparture(int trainNumber, String line, String destination,
                         LocalTime departureTime) {
     this(trainNumber, line, destination, departureTime, -1);
   }
 
+  /**
+   * Gets the train number.
+   *
+   * @return the train number
+   */
   public int getTrainNumber() {
     return trainNumber;
   }
 
+  /**
+   * Gets the line.
+   *
+   * @return the line
+   */
   public String getLine() {
     return line;
   }
 
+  /**
+   * Gets the destination.
+   *
+   * @return the destination
+   */
   public String getDestination() {
     return destination;
   }
 
+  /**
+   * Gets the departure time.
+   *
+   * @return the departure time
+   */
   public LocalTime getDepartureTime() {
     return departureTime;
   }
 
+  /**
+   * Gets the delay.
+   *
+   * @return the delay
+   */
   public LocalTime getDelay() {
     return delay;
   }
 
+  /**
+   * Gets the track.
+   *
+   * @return the track
+   */
   public int getTrack() {
     return track;
   }
 
+  /**
+   * Sets the delay.
+   *
+   * @param delay the delay
+   */
   private void setDelay(LocalTime delay) {
-    if (delay == null) {
-      throw new IllegalArgumentException("Delay cannot be null");
-    }
+    validateDelay(delay);
     this.delay = delay;
   }
 
+  /**
+   * Sets the track.
+   *
+   * @param track the track
+   */
   public void setTrack(int track) {
-    if (track < 0) {
-      throw new IllegalArgumentException("Track cannot be negative");
-    }
+    validateTrack(track);
     this.track = track;
   }
 
+  /**
+   * Returns a string representation of the train departure.
+   *
+   * @return a string representation of the train departure
+   */
   @Override
   public String toString() {
     var sb = new StringBuilder();
@@ -76,7 +137,7 @@ public class TrainDeparture {
     sb.append("Line: ").append(line).append("\n");
     sb.append("Train number: ").append(trainNumber).append("\n");
     sb.append("Destination: ").append(destination).append("\n");
-    if (delay != null) {
+    if (delay != null && delay != LocalTime.of(0, 0)) {
       sb.append("Delay: ").append(delay).append("\n");
     }
     if (track != -1) {
@@ -85,37 +146,79 @@ public class TrainDeparture {
     return sb.toString();
   }
 
-  public void addDelay(LocalTime delay) {
-    setDelay(this.delay.plusHours(delay.getHour()).plusMinutes(delay.getMinute()));
-  }
-
+  /**
+   * Validates the train number.
+   *
+   * @param trainNumber the train number
+   */
   private void validateTrainNumber(int trainNumber) {
-    if (trainNumber < 0) {
+    if (trainNumber <= 0) {
       throw new IllegalArgumentException("Train number cannot be negative");
     }
   }
 
+  /**
+   * Validates the line.
+   *
+   * @param line the line
+   */
   private void validateLine(String line) {
-    if (line == null) {
-      throw new IllegalArgumentException("Line cannot be null");
-    }
     if (line.isBlank()) {
       throw new IllegalArgumentException("Line cannot be blank");
     }
   }
 
+  /**
+   * Validates the destination.
+   *
+   * @param destination the destination
+   */
   private void validateDestination(String destination) {
-    if (destination == null) {
-      throw new IllegalArgumentException("Destination cannot be null");
-    }
     if (destination.isBlank()) {
       throw new IllegalArgumentException("Destination cannot be blank");
     }
   }
 
+  /**
+   * Validates the departure time.
+   *
+   * @param departureTime the departure time
+   */
   private void validateDepartureTime(LocalTime departureTime) {
     if (departureTime == null) {
       throw new IllegalArgumentException("Departure time cannot be null");
     }
+  }
+
+  private void validateTrack(int track) {
+    if (track < -1) {
+      throw new IllegalArgumentException("Track cannot be negative");
+    }
+  }
+
+  /**
+   * Validates the delay.
+   *
+   * @param delay the delay
+   */
+  private void validateDelay(LocalTime delay) {
+    if (delay == null) {
+      throw new IllegalArgumentException("Delay cannot be null");
+    }
+  }
+
+  /**
+   * Adds a delay to the train departure.
+   *
+   * @param delay the delay
+   */
+  public void addDelay(LocalTime delay) {
+    if (delay == null) {
+      throw new IllegalArgumentException("Delay cannot be null");
+    }
+    if (delay.equals(LocalTime.of(0, 0))) {
+      throw new IllegalArgumentException("Delay cannot be zero");
+    }
+    setDelay(this.delay.plusHours(delay.getHour()).plusMinutes(delay.getMinute()));
   }
 }
