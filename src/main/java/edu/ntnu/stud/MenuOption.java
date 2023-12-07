d551b629d6a2c03e81f8424e30c41ea11bd3bcd3
Vetle <vetletb@stud.ntnu.no>
2023-12-08 00:54:08 +0100

    Added constuctor, get and toString for MenuOption

--- src/main/java/edu/ntnu/stud/MenuOption.java
+++ src/main/java/edu/ntnu/stud/MenuOption.java
@@ -1,4 +1,26 @@
 package edu.ntnu.stud;
 
 public class MenuOption {
+  private final String option;
+  private final int choice;
+  private final String command;
+
+  public MenuOption(String option, int choice, String command) {
+    this.option = option;
+    this.choice = choice;
+    this.command = command;
+  }
+
+  public int getChoice() {
+    return choice;
+  }
+
+  public String getCommand() {
+    return command;
+  }
+
+  @Override
+  public String toString() {
+    return "'" + choice + "'" + " -> " + option + ":";
+  }
 }
