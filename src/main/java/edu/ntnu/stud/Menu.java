cd9c1fdd5e8ff25f3fb8a1ceff5250c518765976
Vetle <vetletb@stud.ntnu.no>
2023-12-08 01:09:45 +0100

    Created methods for adding menu option, display and select option for Menu class

--- src/main/java/edu/ntnu/stud/Menu.java
+++ src/main/java/edu/ntnu/stud/Menu.java
@@ -1,4 +1,35 @@
 package edu.ntnu.stud;
 
+import java.util.ArrayList;
+
 public class Menu {
+
+  private String prompt = "Please select an option:";
+  private final ArrayList<MenuOption> menuOptions = new ArrayList<>();
+
+  public void addMenuOption(String option, int choice, String command) {
+    MenuOption menuOption = new MenuOption(option, choice, command);
+    menuOptions.add(menuOption);
+  }
+
+  public void setPrompt(String prompt) {
+    this.prompt = prompt;
+  }
+
+  public void displayMenu() {
+    System.out.println("\n");
+    System.out.println("-".repeat(40));
+    System.out.println(prompt);
+    System.out.println("-".repeat(40));
+    menuOptions.forEach(System.out::println);
+    System.out.println("-".repeat(40));
+  }
+
+  public String select(int choice) {
+    return menuOptions.stream()
+        .filter(options -> options.getChoice() == choice)
+        .findFirst()
+        .map(MenuOption::getCommand)
+        .orElse("");
+  }
 }
