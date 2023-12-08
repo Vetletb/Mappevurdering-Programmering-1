2a04a558e160d26dbfb73c0aec292b7e9b3842ce
Vetle <vetletb@stud.ntnu.no>
2023-12-08 01:11:33 +0100

    Created methods for adding menus and interacting with Menu for MenuBuilder

--- src/main/java/edu/ntnu/stud/MenuBuilder.java
+++ src/main/java/edu/ntnu/stud/MenuBuilder.java
@@ -1,4 +1,32 @@
 package edu.ntnu.stud;
 
+import java.util.HashMap;
+
 public class MenuBuilder {
+  private final HashMap<String, Menu> menus = new HashMap<>();
+
+  public void addMenu(String name) {
+    Menu menu = new Menu();
+    menus.put(name, menu);
+  }
+
+  public void addMenuOption(String menuName, String option, int choice, String command) {
+    Menu menu = menus.get(menuName);
+    menu.addMenuOption(option, choice, command);
+  }
+
+  public void setPrompt(String menuName, String prompt) {
+    Menu menu = menus.get(menuName);
+    menu.setPrompt(prompt);
+  }
+
+  public void displayMenu(String menuName) {
+    Menu menu = menus.get(menuName);
+    menu.displayMenu();
+  }
+
+  public String selectOption(String menuName, int choice) {
+    Menu menu = menus.get(menuName);
+    return menu.select(choice);
+  }
 }
